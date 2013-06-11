ce44295552ae9b745253229668bd4a175f3c19a4
ibudasov <s0m3th1ng>
2013-06-12 00:36:20 +0300

    Предварительная версия классов Запроса и Ответа

--- src/com/company/Request.java
+++ src/com/company/Request.java
@@ -1,5 +1,47 @@
+/**
+ * Класc для получения запросов от пользователя и приведения их
+ * к какому-то стандартному виду, принятому внутри программы
+ *
+ * @version 0.1
+ * @package standard
+ * @author Igor Budasov <igor.budasov@gmail.com>
+ * @copyright Copyright (c) 2013, Igor Budasov
+ */
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Request {
 
+    private String result;
+
+    /**
+     * Конструктор объекта ответа.
+     */
+    public Request() {
+    }
+
+    /**
+     * Зачитываем пользовательский ввод, сохраняем
+     * его в объекте и возвращаем по запросу
+     *
+     * @return String
+     */
+    public String get() {
+        Scanner in = new Scanner(System.in);
+        this.set(in.next());
+        return this.result;
+    }
+
+    /**
+     * Устанавливаем объект запроса.
+     * Метод приватный и может быть вызван только внутри класса
+     *
+     * @param result
+     * @return
+     */
+    private String set(String result) {
+        return this.result = result;
+    }
 }
--- src/com/company/Response.java
+++ src/com/company/Response.java
@@ -1,4 +1,52 @@
+/**
+ * Класc для отсылки результатов работы юзеру
+ *
+ * @version 0.1
+ * @package standard
+ * @author Igor Budasov <igor.budasov@gmail.com>
+ * @copyright Copyright (c) 2013, Igor Budasov
+ */
+
 package com.company;
 
 public class Response {
+
+    private String result;
+
+    /**
+     * Конструктор объекта ответа
+     */
+    Response() {
+
+    }
+
+    /**
+     * Метод возвращает текущее содержимое объекта запроса
+     *
+     * @return String
+     */
+    public String get() {
+        return this.result;
+    }
+
+    /**
+     * Метод устанавливает содержимое ответа.
+     * Например, когда мы обработали какие-то данные --
+     * мы можем установить их в качестве ответа и отдать юзеру
+     * с помощью Response.send();
+     *
+     * @param result String
+     * @return String
+     */
+    public String set(String result) {
+        return this.result = result;
+    }
+
+    /**
+     * Метод отправляет пользователю ответ программы
+     */
+    public void send() {
+        System.out.println(this.result);
+        return;
+    }
 }
