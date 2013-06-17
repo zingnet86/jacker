e0dbd453aada1ab56b434122935721a863e8141e
roman.malinkin <zingnet86@gmail.com>
2013-06-17 16:14:43 +0300

    Обновил класс Storage

--- src/com/company/Storage.java
+++ src/com/company/Storage.java
@@ -1,46 +1,102 @@
-/**
- * Класc для хранения данных о задачах
- *
- * @version 0.1
- * @package standard
- * @author Igor Budasov <igor.budasov@gmail.com>
- * @copyright Copyright (c) 2013, Igor Budasov
- */
-
-package com.company;
+﻿package com.company;
 
 public class Storage {
-
+	 
     /**
      * Двумерный массив с задачами.
-     * Ключ записи - айдишник задачи.
-     * Поля записи - времчя старта и финиша, имя задачи
+     * Ключ записи - <i> задачи.
+     * Поля записи - <a> старта и финиша, имя задачи
      */
     public String[][] tasks;
-
+ 
+    
     public Storage() {
         // конструктор
-        // this.tasks[1] = new String[]{"Название задачи", "2013-10-10 11:12:13", "2013-10-10 11:12:14"};
-        return;
+    	
+		this.tasks = new String[100][];
+		
+		this.tasks[0] = new String[]{"Название задачи", "2013-10-10 11:12:13", "2013-10-10 11:12:14"};
     }
-
-    public String[] add() {
-        // добавление таска в переменную tasks
-        return new String[]{};
+    
+ 
+    public String[] add(String[] task) {
+        // добавление таска
+    	
+    	String[][] data = getTasks();
+    	
+    	for(int i = 0; i < data.length; i++)
+    	{
+    		if(data[i] == null)
+    		{
+    			data[i] = task;
+    			
+    			break;
+    		}
+    	}
+    	
+    	store(data);
+    	
+    	return  task;
     }
-
+    
+ 
     public String[] get(Integer id) {
-        // вохвращает задачу по айдишнику из переменноцй tasks
-        return new String[]{};
+        // вохвращает задачу по айдишнику
+    	
+    	String[][] data = getTasks();
+    	
+        return  data[id];
     }
-
-    public String[] delete(Integer id) {
-        // удаление таска по айдишнику из переменноцй tasks
-        return new String[]{};
+    
+    
+    public int getIdByName(String name) {
+        // вохвращает id задачи по имени
+    	String[][] data = getTasks();
+    	
+    	for(int i = 0; i < data.length; i++)
+    	{
+    		if(data[i][0] == name)
+    			return i;
+    	}
+    	
+        return -1;
     }
-
-    public String[] truncate(Integer id) {
+    
+ 
+    public boolean delete(Integer id) {
+        // удаление таска по айдишнику
+    	
+    	String[][] data = getTasks();
+    	
+    	tasks[id] = new String[]{};
+    	
+    	store(data);
+    	
+        return true;
+    }
+    
+ 
+    public boolean truncate() {
         // удаление всех задач
-        return new String[]{};
+    	
+    	store(new String[100][]);
+    	
+        return true;
+    }
+    
+    
+    private String[][] getTasks() {
+    	//получение данных
+    	
+    	return tasks;
+    }
+    
+	
+	public void store(String[][] data) {
+		//сохранение данных
+		
+		data = this.tasks;
     }
 }
+
+
